aed40b51ba0db2441b679373ec3f3761af4ed417
nitonise <cozmicdust@proton.me>
2024-02-20 14:46:14 +0300

    Upd jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,7 +1,21 @@
+import ActionButton from "./ActionButton";
+
 const Jumbotron = () => {
     return (
-        <section>
-            <h2>Jumbotron</h2>
+        <section className="jumbotron">
+            <div className="jumbotron__content">
+                <div className="jumbotron__desc">
+                    <h1 className="jumbotron__title title">Little Lemon</h1>
+                    <h2 className="subtitle">Chicago</h2>
+                    <p className="jumbotron__text call-to-action">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Assumenda quae nulla esse cupiditate,
+                        laboriosam dicta ducimus excepturi aut molestiae natus possimus qui.
+                    </p>
+                    <ActionButton title="Reserve a table" />
+                </div>
+                <img className="jumbotron__img" src="/images/restauranfood.jpg" alt="some image"/>
+            </div>
         </section>
     );
 };
