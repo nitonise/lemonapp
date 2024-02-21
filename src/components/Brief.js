24bcc7fc80a33def89e38dd066a6ba08b09a6ca5
nitonise <cozmicdust@proton.me>
2024-02-21 14:11:12 +0300

    Upd homepage

--- src/components/Brief.js
+++ src/components/Brief.js
@@ -1,7 +1,19 @@
 const Brief = () => {
     return (
-        <section>
-            <h2>Brief</h2>
+        <section className="brief">
+            <div className="brief__text-content">
+                <h2 className="title">Little Lemon</h2>
+                <h3 className="subtitle">Chicago</h3>
+                <p className="brief__paragraph paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, exercitationem corporis dolores recusandae asperiores doloremque mollitia adipisci voluptatibus nihil aspernatur laudantium tempore? Molestiae in assumenda temporibus laboriosam aperiam eum hic?
+                    Magni odio praesentium tempore! Eligendi a ipsa deleniti architecto vitae. Exercitationem, natus eum a perspiciatis ad eos corporis accusamus reprehenderit illum quia ipsum saepe? Voluptatem est aspernatur maxime repellat expedita!
+                    Eaque est perferendis ratione soluta ipsam tempora suscipit voluptatum. Dolores, inventore earum pariatur doloribus nostrum temporibus eius eveniet cum itaque sunt, sed ullam dolorem deserunt in, doloremque ut adipisci aut.
+                </p>
+            </div>
+            <div className="brief__img-content">
+                <img className="brief__img-1" src="/images/owners1.jpg" alt="Owners" />
+                <img className="brief__img-2" src="/images/owners1.jpg" alt="Owners" />
+            </div>
         </section>
     );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,41 +39,43 @@ const toNavItem = (link) => {
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__logo footer__section">
-                <Link className="footer__logo-link" to="/">
-                    <img className="footer__logo-img" src="/images/logo_mono_v.svg" alt="logo" />
-                </Link>
-            </div>
-            <nav className="footer__section">
-                <h2 className="section-title">Doormat Navigation</h2>
-                <ul className="footer__list">
-                    { links.map(toNavItem) }
-                </ul>
-            </nav>
-            <div className="footer__section">
-                <h2 className="section-title">Contacts</h2>
-                <ul className="footer__list">
-                    <li className="paragraph">
-                        <a className="footer__list-item-link" href="#tel:+1234567890">+1234567890</a>
-                    </li>
-                    <li className="paragraph">
-                        <a className="footer__list-item-link" href="#mailto:little.lemon@box.com">little.lemon@box.com</a>
-                    </li>
-                    <li className="paragraph">
-                        <span>Lorem Ipsum Street, 12</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer__section">
-                <h2 className="section-title">Social Media</h2>
-                <ul className="footer__list">
-                    <li className="paragraph">
-                        <a className="footer__list-item-link" href="#facebook.com">FB</a>
-                    </li>
-                    <li className="paragraph">
-                        <a className="footer__list-item-link" href="#instagram.com">IG</a>
-                    </li>
-                </ul>
+            <div className="footer__content">
+                <div className="footer__logo footer__section">
+                    <Link className="footer__logo-link" to="/">
+                        <img className="footer__logo-img" src="/images/logo_mono_v.svg" alt="logo" />
+                    </Link>
+                </div>
+                <nav className="footer__section">
+                    <h2 className="section-title">Doormat Navigation</h2>
+                    <ul className="footer__list">
+                        { links.map(toNavItem) }
+                    </ul>
+                </nav>
+                <div className="footer__section">
+                    <h2 className="section-title">Contacts</h2>
+                    <ul className="footer__list">
+                        <li className="paragraphd">
+                            <a className="footer__list-item-link" href="#tel:+1234567890">+1234567890</a>
+                        </li>
+                        <li className="paragraph">
+                            <a className="footer__list-item-link" href="#mailto:little.lemon@box.com">little.lemon@box.com</a>
+                        </li>
+                        <li className="paragraph">
+                            <span>Lorem Ipsum Street, 12</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="footer__section">
+                    <h2 className="section-title">Social Media</h2>
+                    <ul className="footer__list">
+                        <li className="paragraph">
+                            <a className="footer__list-item-link" href="#facebook.com">Facebook</a>
+                        </li>
+                        <li className="paragraph">
+                            <a className="footer__list-item-link" href="#instagram.com">Instagram</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
