edf4a4d2ac635f4e2cc04781dc3696f67ebc8b11
nitonise <cozmicdust@proton.me>
2024-02-22 22:21:28 +0300

    Add test

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from './BookingForm';
 
 test("Renders the BookingForm 'setup' field", () => {
     render(
         <BookingForm
             availableTimes={[]}
-            updateTimes={() => {}}
+            updateTimes={jest.fn()}
         />
     );
 
@@ -17,10 +17,26 @@ test("Renders the BookingForm 'personal info' field", () => {
     render(
         <BookingForm
             availableTimes={[]}
-            updateTimes={() => {}}
+            updateTimes={jest.fn()}
         />
     );
 
     const headingElement = screen.getByText(/personal info/i);
     expect(headingElement).toBeInTheDocument();
+})
+
+test("Form submit triggers updateTimes", () => {
+    const updateTimes = jest.fn();
+
+    render(
+        <BookingForm
+            availableTimes={[]}
+            updateTimes={updateTimes}
+        />
+    );
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton)
+
+    expect(updateTimes).toHaveBeenCalledTimes(1);
 })
