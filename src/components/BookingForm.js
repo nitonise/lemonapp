8094ff09e7b87bc258ac5d92ece5b994a5c13818
nitonise <cozmicdust@proton.me>
2024-02-25 11:44:49 +0300

    Add js validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,25 +1,75 @@
 import { useState } from "react";
 
+const validateDate = date => {
+    const curDate = new Date().toISOString().substr(0, 10);
+    return date >= curDate;
+};
+
+const validateTime = time => {
+    return !!time.trim();
+};
+
+const validateGuests = num => {
+    return num >= 1 && num <= 10;
+};
+
+const validateOccasion = val => {
+    return !!val.trim();
+};
+
+const validateFirstName = name => {
+    return !!name.trim() && name.length <= 50;
+};
+
+const validateLastName = name => {
+    return !!name.trim() && name.length <= 50;
+};
+
+const validateEmail = email => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return !!email.trim() && email.match(emailRegex);
+}
+
+const ErrMessage = ({ text }) => {
+    return <strong className="err-msg highlight">*{ text }</strong>;
+};
+
 const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
-    const [time, setTime] = useState("");
-    const [date, setDate] = useState("");
-    const [guests, setGuests] = useState("");
-    const [occasion, setOccasion] = useState("");
-    const [name, setName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [tel, setTel] = useState("");
+    const [time, setTime] = useState({ value: "", isTouched: false, });
+    const [date, setDate] = useState({ value: "", isTouched: false, });
+    const [guests, setGuests] = useState({ value: "", isTouched: false, });
+    const [occasion, setOccasion] = useState({ value: "", isTouched: false, });
+    const [firstName, setFirstName] = useState({ value: "", isTouched: false, });
+    const [lastName, setLastName] = useState({ value: "", isTouched: false, });
+    const [email, setEmail] = useState({ value: "", isTouched: false, });
+    const [tel, setTel] = useState({ value: "", isTouched: false, });
+
+    const isFormValid = () => {
+        return validateDate(date.value)
+            && validateTime(time.value)
+            && validateGuests(guests.value)
+            && validateOccasion(occasion.value)
+            && validateFirstName(firstName.value)
+            && validateLastName(lastName.value)
+            && validateEmail(email.value);
+    }
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        if (!isFormValid()) {
+            alert("An invalid form cannot be submitted.\Please check the form and try again.");
+            return;
+        }
+
         const formData = {
-            date,
-            time,
-            guests,
-            occasion,
-            name,
-            email,
-            tel,
+            date: date.value,
+            time: time.value,
+            guests: guests.value,
+            occasion: occasion.value,
+            firstName: firstName.value,
+            email: email.value,
+            tel: tel.value,
         };
 
         submitForm(formData);
@@ -43,13 +93,19 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     id="res-date"
                     min={ currentDate }
                     required
-                    value={ date }
+                    value={ date.value }
                     onChange={e => {
-                        const date = e.target.value;
-                        setDate(date);
+                        setDate({ ...date, value: e.target.value });
                         updateTimes(date);
                     }}
+                    onBlur = { () => setDate({ ...date, isTouched: true }) }
                 />
+                {(
+                    date.isTouched && !validateDate(date.value)
+                    ? <ErrMessage text={ "Indicate the date, set current or later" } />
+                    : null
+                )}
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="res-time"
@@ -59,13 +115,20 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                 <select
                     className="booking-fm__in call-to-action"
                     id="res-time"
-                    value={ time }
-                    onChange={ e => setTime(e.target.value) }
+                    value={ time.value }
+                    onChange={ e => setTime({ ...time, value: e.target.value }) }
+                    onBlur={ () => setTime({ ...time, isTouched: true }) }
                     required
                 >
                     <option value=""></option>
                     { availableTimes.map(t => <option key={t}>{t}</option>) }
                 </select>
+                {(
+                    time.isTouched && !validateTime(time.value)
+                    ? <ErrMessage text={ "Indicate the time, choose among available ones" } />
+                    : null
+                )}
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="guests"
@@ -78,10 +141,18 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     min="1"
                     max="10"
                     id="guests"
-                    value={ guests }
+                    value={ guests.value }
                     required
-                    onChange={ e => setGuests(e.target.value) }
+                    onChange={ e => setGuests({ ...guests, value: e.target.value }) }
+                    onBlur={ () => setGuests({ ...guests, isTouched: true }) }
                 />
+                {(
+                    guests.isTouched && !validateGuests(guests.value)
+                    ? <ErrMessage text={ "Indicate the number of guests, from 1 to 10" } />
+                    : null
+                )}
+
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="occasion"
@@ -91,14 +162,21 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                 <select
                     className="booking-fm__in call-to-action"
                     id="occasion"
-                    value={ occasion }
-                    onChange={ e => setOccasion(e.target.value) }
+                    value={ occasion.value }
+                    onChange={ e => setOccasion({ ...occasion, value: e.target.value }) }
                     required
+                    onBlur={ () => setOccasion({ ...occasion, isTouched: true }) }
                 >
                     <option value=""></option>
                     <option>Birthday</option>
                     <option>Anniversary</option>
                 </select>
+                {(
+                    occasion.isTouched && !validateOccasion(occasion.value)
+                    ? <ErrMessage text={ "Indicate the occasion, choose among available ones" } />
+                    : null
+                )}
+
             </fieldset>
             <fieldset className="booking-fm__field">
                 <legend className="section-title-up">Personal info</legend>
@@ -112,11 +190,18 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     className="booking-fm__in call-to-action"
                     type="text"
                     id="res-name"
-                    value={ name }
-                    onChange={ e => setName(e.target.value) }
+                    value={ firstName.value }
+                    onChange={ e => setFirstName({ ...firstName, value: e.target.value }) }
+                    onBlur={ () => setFirstName({ ...firstName, isTouched: true }) }
                     maxLength="50"
                     required
                 />
+                {(
+                    firstName.isTouched && !validateFirstName(firstName.value)
+                    ? <ErrMessage text={ "Indicate your first name" } />
+                    : null
+                )}
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="res-last-name"
@@ -127,11 +212,18 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     className="booking-fm__in call-to-action"
                     type="text"
                     id="res-last-name"
-                    value={ lastName }
-                    onChange={ e => setLastName(e.target.value) }
+                    value={ lastName.value }
+                    onChange={ e => setLastName({ ...lastName, value: e.target.value }) }
+                    onBlur={ () => setLastName({ ...lastName, isTouched: true }) }
                     maxLength="50"
                     required
                 />
+                {(
+                    lastName.isTouched && !validateLastName(lastName.value)
+                    ? <ErrMessage text={ "Indicate your last name" } />
+                    : null
+                )}
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="res-email"
@@ -142,10 +234,17 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     className="booking-fm__in call-to-action"
                     type="email"
                     id="res-email"
-                    value={ email }
-                    onChange={ e => setEmail(e.target.value) }
+                    value={ email.value }
+                    onChange={ e => setEmail({ ...email, value: e.target.value }) }
+                    onBlur={ () => setEmail({ ...email, isTouched: true }) }
                     required
                 />
+                {(
+                    email.isTouched && !validateEmail(email.value)
+                    ? <ErrMessage text={ "Indicate your email" } />
+                    : null
+                )}
+
                 <label
                     className="booking-fm__lbl section-category"
                     htmlFor="res-tel"
@@ -156,8 +255,9 @@ const BookingForm = ({ availableTimes, updateTimes, submitForm }) => {
                     className="booking-fm__in call-to-action"
                     type="tel"
                     id="res-tel"
-                    value={ tel }
-                    onChange={ e => setTel(e.target.value) }
+                    value={ tel.value }
+                    onChange={ e => setTel({ ...tel, value: e.target.value }) }
+                    onBlur = { () => setTel({ ...tel, isTouched: true }) }
                 />
             </fieldset>
             <input
