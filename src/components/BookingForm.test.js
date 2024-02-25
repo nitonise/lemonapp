7b63cc939b05042330a6e03da900ff512d08abb0
nitonise <cozmicdust@proton.me>
2024-02-25 19:16:28 +0300

    Upd tests

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,43 +1,72 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from './BookingForm';
 
-test("Renders the BookingForm 'setup' field", () => {
-    render(
-        <BookingForm
-            availableTimes={[]}
-            updateTimes={jest.fn()}
-        />
-    );
+describe("Renders fieldsets", () => {
+    test("Setup", () => {
+        render(
+            <BookingForm
+                availableTimes={[]}
+                updateTimes={jest.fn()}
+            />
+        );
 
-    const headingElement = screen.getByText(/setup/i);
-    expect(headingElement).toBeInTheDocument();
-})
+        const headingElement = screen.getByText(/setup/i);
+        expect(headingElement).toBeInTheDocument();
+    })
 
-test("Renders the BookingForm 'personal info' field", () => {
-    render(
-        <BookingForm
-            availableTimes={[]}
-            updateTimes={jest.fn()}
-        />
-    );
+    test("Personal info", () => {
+        render(
+            <BookingForm
+                availableTimes={[]}
+                updateTimes={jest.fn()}
+            />
+        );
 
-    const headingElement = screen.getByText(/personal info/i);
-    expect(headingElement).toBeInTheDocument();
-})
+        const headingElement = screen.getByText(/personal info/i);
+        expect(headingElement).toBeInTheDocument();
+    })
+});
 
-test("Form is submitted", () => {
-    const submitForm = jest.fn();
+describe("Submits the form", () => {
+    test("Invalid form", () => {
+        const submitForm = jest.fn();
 
-    render(
-        <BookingForm
-            availableTimes={[]}
-            updateTimes={jest.fn()}
-            submitForm = {submitForm}
-        />
-    );
+        render(
+            <BookingForm
+                availableTimes={[]}
+                updateTimes={jest.fn()}
+                submitForm = {submitForm}
+            />
+        );
 
-    const submitButton = screen.getByRole("button");
-    fireEvent.click(submitButton)
+        const submitButton = screen.getByRole("button");
+        fireEvent.click(submitButton)
 
-    expect(submitForm).toHaveBeenCalledTimes(0);
-})
+        expect(submitForm).toHaveBeenCalledTimes(0);
+    });
+
+    test("Valid form", () => {
+        const submitForm = jest.fn();
+
+        render(
+            <BookingForm
+                availableTimes={["10:00"]}
+                updateTimes={jest.fn()}
+                submitForm = {submitForm}
+            />
+        );
+
+        const curDate = new Date().toISOString().substr(0, 10);
+        fireEvent.change(screen.getByLabelText(/choose date/i), {target: {value: curDate}});
+        fireEvent.change(screen.getByLabelText(/choose time/i), {target: {value: "10:00"}});
+        fireEvent.change(screen.getByLabelText(/number of guests/i), {target: {value: 2}});
+        fireEvent.change(screen.getByLabelText(/occasion/i), {target: {value: "Birthday"}});
+        fireEvent.change(screen.getByLabelText(/first name/i), {target: {value: "John"}});
+        fireEvent.change(screen.getByLabelText(/last name/i), {target: {value: "Doe"}});
+        fireEvent.change(screen.getByLabelText(/email/i), {target: {value: "jd@jd.jd"}});
+
+        fireEvent.click(screen.getByRole("button"))
+
+        expect(submitForm).toHaveBeenCalledTimes(1);
+    });
+});
